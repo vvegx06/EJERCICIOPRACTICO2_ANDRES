78e22ae708ca3fd458c5ec55c8778f190fe17792
Andres Vega <andresvegamunoz2206@gmail.com>
2025-08-14 00:03:25 -0600

    commit templates

--- examen2_andres/src/main/resources/static/rutinas.js
+++ examen2_andres/src/main/resources/static/rutinas.js
@@ -4,3 +4,23 @@
  */
 
 
+function readURL(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+        reader.onload = function (e) {
+            $('#blah')
+                    .attr('src', e.target.result)
+                    .height(200);
+        };
+        reader.readAsDataURL(input.files[0]);
+    }
+}
+
+/* La siguiente función se utiliza para activar la cantidad de elementos seleccionados 
+ en el carrito de compras utilizando un llamado "ajax" */
+function addCard(formulario) {
+    var valor = formulario.elements[0].value;
+    var url = '/carrito/agregar';
+    url = url + '/' + valor;
+    $("#resultsBlock").load(url);
+}
